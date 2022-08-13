3377005566fcdec13945c3d2c1cb6561d3806292
Filmon Arefayne <filmon.arefayne@protonmail.com>
2022-08-13 21:56:19 +0200

    update init.el

--- init.el
+++ init.el
@@ -7,4 +7,54 @@
 
 (global-display-line-numbers-mode 1)
 
-(load-theme 'modus-vivendi' t)		
+(recentf-mode 1)
+
+(setq history-length 25)
+(savehist-mode 1)
+
+
+(save-place-mode 1)
+
+(setq custom-file (locate-user-emacs-file "custom-vars.el"))
+(load custom-file 'noerror 'nomessage)
+
+(setq use-dialog-box nil)
+
+(global-auto-revert-mode 1)
+
+(setq global-auto-revert-non-file-buffers t)
+(setq global-auto-revert-non-file-buffers t)
+
+
+(setq modus-themes-mode-line '(accented borderless padded))
+(setq modus-themes-region '(bg-only))
+(setq modus-themes-completions 'opinionated)
+(setq modus-themes-bold-constructs t)
+
+
+(load-theme 'modus-vivendi' t)
+
+
+(defun duplicate-line-or-region (&optional n)
+      "Duplicate current line, or region if active.
+    With argument N, make N copies.
+    With negative N, comment out original line and use the absolute value."
+      (interactive "*p")
+      (let ((use-region (use-region-p)))
+        (save-excursion
+          (let ((text (if use-region        ;Get region if active, otherwise line
+                          (buffer-substring (region-beginning) (region-end))
+                        (prog1 (thing-at-point 'line)
+                          (end-of-line)
+                          (if (< 0 (forward-line 1)) ;Go to beginning of next line, or make a new one
+                              (newline))))))
+            (dotimes (i (abs (or n 1)))     ;Insert N times, or once if not specified
+              (insert text))))
+        (if use-region nil                  ;Only if we're working with a line (not a region)
+          (let ((pos (- (point) (line-beginning-position)))) ;Save column
+            (if (> 0 n)                             ;Comment out original with negative arg
+                (comment-region (line-beginning-position) (line-end-position)))
+            (forward-line 1)
+            (forward-char pos)))))
+
+(global-set-key (kbd "C-d") 'duplicate-line-or-region)
